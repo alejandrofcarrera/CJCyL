96624f68c51924768f86511fccbc890376ef0f1f
Alejandro Fdez Carrera <alej4fc@gmail.com>
2015-01-17 15:13:56 +0100

    add init js and update layout

---
+++ source/js/init.js
@@ -0,0 +1,21 @@
+
+'use strict';
+
+var toogleFacet = function toogleFacet(e) {
+  if ($(e.currentTarget).hasClass('on')) {
+    $(e.currentTarget).removeClass('on');
+  } else {
+    $(e.currentTarget).addClass('on');
+  }
+};
+
+$(document).ready(function () {
+  $('.facet').click(toogleFacet);
+	var mapOptions = {
+		center: new google.maps.LatLng(41.6551800, -4.7237200),
+		zoom: 7,
+		disableDefaultUI: true,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	map = new google.maps.Map(document.getElementById("rightPanelMap"), mapOptions);
+});
